213611aedb841a0754c9108a5afde30c9ee1882d
G M Abhilash <abhilashmg5882@gmail.com>
2022-01-18 18:46:37 +0530

    complete code

---
+++ src/main/java/com/jspiders/dockercomposefileapp/DockerComposeFileAppApplication.java
@@ -0,0 +1,13 @@
+package com.jspiders.dockercomposefileapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerComposeFileAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerComposeFileAppApplication.class, args);
+	}
+
+}
